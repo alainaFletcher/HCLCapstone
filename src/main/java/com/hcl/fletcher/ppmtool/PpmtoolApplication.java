cfbb023a30d8cbbd25312f80aa9ec45b9ae2f048
alainaFletcher <bebebeanboi@gmail.com>
2022-01-20 11:41:45 -0500

    first commit

---
+++ src/main/java/com/hcl/fletcher/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.fletcher.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
