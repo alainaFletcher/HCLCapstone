835fc172349b9c3b0b37a456cb5acf94f5fe8779
alainaFletcher <bebebeanboi@gmail.com>
2022-01-22 20:45:39 -0500

    more project more backlog but this is branch 33

--- src/main/java/com/hcl/fletcher/ppmtool/domain/ProjectTask.java
+++ src/main/java/com/hcl/fletcher/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,9 @@
 package com.hcl.fletcher.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -19,6 +23,10 @@ public class ProjectTask {
     private Integer priority;
     private Date dueDate;
     //ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name="backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
 
     @Column(updatable = false)
     private String projectIdentifer;
@@ -106,6 +114,14 @@ public class ProjectTask {
 
     public void setUpdate_At(Date update_At) {
         this.update_At = update_At;
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @PrePersist
